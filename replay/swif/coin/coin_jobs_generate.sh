e218fd32a807943abb1c0adc709bb1445872ba23
John Matter <jcm6fv@virginia.edu>
2018-12-06 23:03:39 -0500

    Update replay swif job generator script

--- replay/swif/coin/coin_jobs_generate.sh
+++ replay/swif/coin/coin_jobs_generate.sh
@@ -4,10 +4,9 @@
 # Hopefully this will make re-replaying easy when changes are made
 # to hcana or hallc_replay.
 
-
-hcswif_dir=/home/jmatter/CTswif/hcswif
+hcswif_dir=/home/jmatter/ctswif/
 runlist_dir=/home/jmatter/ct_scripts/runlists/coin
 for f in `ls $runlist_dir/runs_*Q2_*`; do
     f_base=`basename $f`
-    $hcswif_dir/hcswif.py --mode replay --spectrometer COIN --replay SCRIPTS/COIN/PRODUCTION/replay_production_coin_hElec_pProt.C --events -1 --project c-comm2017 --run file $f --name $f_base
+    $hcswif_dir/hcswif.py --mode replay --spectrometer COIN --replay SCRIPTS/COIN/PRODUCTION/replay_production_ct.C --events -1 --project c-comm2017 --run file $f --name $f_base
 done
