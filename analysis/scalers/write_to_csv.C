3240c0efac24ce20718525b1bfcb894ce4cef1f9
John Matter <jcm6fv@virginia.edu>
2019-06-10 14:27:41 -0400

    Add BCMs' charge, chargecut, and current; add ptrig3 and ptrig4

--- analysis/scalers/write_to_csv.C
+++ analysis/scalers/write_to_csv.C
@@ -12,12 +12,13 @@
 
 // Print bcm currents, charges, and pTRIG1 rate
 void write_to_csv() {
-    CTData *data = new CTData("/home/jmatter/ct_scripts/ct_coin_data.json");
+    CTData *data = new CTData("/home/jmatter/ct_scripts/ct_coin_data_pass2_newtrack.json");
     CTCuts *cuts = new CTCuts("/home/jmatter/ct_scripts/cuts.json");
 
     // Which kinematics
-    std::vector<TString> kinematics = {"LH2_Q2_8","LH2_Q2_10","LH2_Q2_12","LH2_Q2_14",
-                                       "C12_Q2_8","C12_Q2_10","C12_Q2_12","C12_Q2_14"};
+    std::vector<TString> kinematics = data->GetNames();
+    // std::vector<TString> kinematics = {"LH2_Q2_8","LH2_Q2_10","LH2_Q2_12","LH2_Q2_14",
+    //                                    "C12_Q2_8","C12_Q2_10","C12_Q2_12","C12_Q2_14"};
 
     // Where do we save the debug data?
     TString csvFilename = "/home/jmatter/work_jmatter/csv/ct_rates.csv";
@@ -51,13 +52,13 @@ void write_to_csv() {
 
     // ------------------------------------------------------------------------
     // Loop over files and print all entries in the tree
-    Double_t bcm1;
-    Double_t bcm2;
-    Double_t bcm4a;
-    Double_t bcm4b;
-    Double_t bcm4c;
-    Double_t hTRIG1;
-    Double_t pTRIG1;
+    Double_t bcm1current,  bcm1charge,  bcm1chargecut;
+    Double_t bcm2current,  bcm2charge,  bcm2chargecut;
+    Double_t bcm4Acurrent, bcm4Acharge, bcm4Achargecut;
+    Double_t bcm4Bcurrent, bcm4Bcharge, bcm4Bchargecut;
+    Double_t bcm4Ccurrent, bcm4Ccharge, bcm4Cchargecut;
+    Double_t Unsercurrent, Unsercharge, Unserchargecut;
+    Double_t pTRIG1, pTRIG3, pTRIG4;
 
     std::ofstream csv;
     csv.open(csvFilename.Data());
@@ -66,13 +67,27 @@ void write_to_csv() {
         << "," << "target"
         << "," << "run"
         << "," << "i"
-        << "," << "bcm1"
-        << "," << "bcm2"
-        << "," << "bcm4a"
-        << "," << "bcm4b"
-        << "," << "bcm4c"
-        << "," << "hTRIG1"
+        << "," << "bcm1current"
+        << "," << "bcm1charge"
+        << "," << "bcm1chargecut"
+        << "," << "bcm2current"
+        << "," << "bcm2charge"
+        << "," << "bcm2chargecut"
+        << "," << "bcm4Acurrent"
+        << "," << "bcm4Acharge"
+        << "," << "bcm4Achargecut"
+        << "," << "bcm4Bcurrent"
+        << "," << "bcm4Bcharge"
+        << "," << "bcm4Bchargecut"
+        << "," << "bcm4Ccurrent"
+        << "," << "bcm4Ccharge"
+        << "," << "bcm4Cchargecut"
+        << "," << "Unsercurrent"
+        << "," << "Unsercharge"
+        << "," << "Unserchargecut"
         << "," << "pTRIG1"
+        << "," << "pTRIG3"
+        << "," << "pTRIG4"
         << std::endl;
 
     std::cout << "Get charge---------------------" << std::endl;
@@ -92,13 +107,33 @@ void write_to_csv() {
             TTree *scalerTree = (TTree*) f->Get("TSP");
 
             std::cout<<"set branches for run " << run;
-            scalerTree->SetBranchAddress("P.BCM1.scalerCurrent",  &bcm1);
-            scalerTree->SetBranchAddress("P.BCM2.scalerCurrent",  &bcm2);
-            scalerTree->SetBranchAddress("P.BCM4A.scalerCurrent", &bcm4a);
-            scalerTree->SetBranchAddress("P.BCM4B.scalerCurrent", &bcm4b);
-            scalerTree->SetBranchAddress("P.BCM4C.scalerCurrent", &bcm4c);
-            scalerTree->SetBranchAddress("P.hTRIG1.scalerRate",   &hTRIG1);
+            scalerTree->SetBranchAddress("P.BCM1.scalerCurrent",    &bcm1current);
+            scalerTree->SetBranchAddress("P.BCM1.scalerCharge",     &bcm1charge);
+            scalerTree->SetBranchAddress("P.BCM1.scalerChargeCut",  &bcm1chargecut);
+
+            scalerTree->SetBranchAddress("P.BCM2.scalerCurrent",    &bcm2current);
+            scalerTree->SetBranchAddress("P.BCM2.scalerCharge",     &bcm2charge);
+            scalerTree->SetBranchAddress("P.BCM2.scalerChargeCut",  &bcm2chargecut);
+
+            scalerTree->SetBranchAddress("P.BCM4A.scalerCurrent",    &bcm4Acurrent);
+            scalerTree->SetBranchAddress("P.BCM4A.scalerCharge",     &bcm4Acharge);
+            scalerTree->SetBranchAddress("P.BCM4A.scalerChargeCut",  &bcm4Achargecut);
+
+            scalerTree->SetBranchAddress("P.BCM4B.scalerCurrent",    &bcm4Bcurrent);
+            scalerTree->SetBranchAddress("P.BCM4B.scalerCharge",     &bcm4Bcharge);
+            scalerTree->SetBranchAddress("P.BCM4B.scalerChargeCut",  &bcm4Bchargecut);
+
+            scalerTree->SetBranchAddress("P.BCM4C.scalerCurrent",    &bcm4Ccurrent);
+            scalerTree->SetBranchAddress("P.BCM4C.scalerCharge",     &bcm4Ccharge);
+            scalerTree->SetBranchAddress("P.BCM4C.scalerChargeCut",  &bcm4Cchargecut);
+
+            scalerTree->SetBranchAddress("P.Unser.scalerCurrent",    &Unsercurrent);
+            scalerTree->SetBranchAddress("P.Unser.scalerCharge",     &Unsercharge);
+            scalerTree->SetBranchAddress("P.Unser.scalerChargeCut",  &Unserchargecut);
+
             scalerTree->SetBranchAddress("P.pTRIG1.scalerRate",   &pTRIG1);
+            scalerTree->SetBranchAddress("P.pTRIG3.scalerRate",   &pTRIG3);
+            scalerTree->SetBranchAddress("P.pTRIG4.scalerRate",   &pTRIG4);
             std::cout<<"✓"<<std::endl;
 
             // Print branches to csv for debugging
@@ -111,13 +146,27 @@ void write_to_csv() {
                     << "," << target
                     << "," << run
                     << "," << i
-                    << "," << bcm1
-                    << "," << bcm2
-                    << "," << bcm4a
-                    << "," << bcm4b
-                    << "," << bcm4c
-                    << "," << hTRIG1
+                    << "," << bcm1current
+                    << "," << bcm1charge
+                    << "," << bcm1chargecut
+                    << "," << bcm2current
+                    << "," << bcm2charge
+                    << "," << bcm2chargecut
+                    << "," << bcm4Acurrent
+                    << "," << bcm4Acharge
+                    << "," << bcm4Achargecut
+                    << "," << bcm4Bcurrent
+                    << "," << bcm4Bcharge
+                    << "," << bcm4Bchargecut
+                    << "," << bcm4Ccurrent
+                    << "," << bcm4Ccharge
+                    << "," << bcm4Cchargecut
+                    << "," << Unsercurrent
+                    << "," << Unsercharge
+                    << "," << Unserchargecut
                     << "," << pTRIG1
+                    << "," << pTRIG3
+                    << "," << pTRIG4
                     << std::endl;
             }
 
