5534d022d3477f9b0441e115817c13bb92ca6ee0
John Matter <jcm6fv@virginia.edu>
2019-12-18 17:24:30 -0500

    Modify hadd script

--- replay/scripts/CT_hadd.py
+++ replay/scripts/CT_hadd.py
@@ -8,27 +8,38 @@ Q2s = [8,10,12,14]
 targets = ['LH2', 'C12', 'AL']
 
 # Where are the lists of run numbers by kinematics?
-runlistDir = '/home/jmatter/ct_scripts/runlists'
+runlistDir = '/home/jmatter/ct_scripts/runlists/coin'
 
-# Run list filename template
-runlistTemplate = 'runs_%s_Q2_%d'
+# Run lists to process, and the filename to use for the hadd-ed files
+haddFilename = {}
+haddFilename['runs_C12_Q2_8_thick']             = 'coin_replay_production_C12_thick_8_smallcoll.root'
+haddFilename['runs_C12_Q2_8_thin']              = 'coin_replay_production_C12_thin_8_smallcoll.root'
+haddFilename['runs_C12_Q2_10_thick']            = 'coin_replay_production_C12_thick_9.5_largecoll.root'
+haddFilename['runs_C12_Q2_10_thin']             = 'coin_replay_production_C12_thin_9.5_largecoll.root'
+haddFilename['runs_C12_Q2_12']                  = 'coin_replay_production_C12_thick_11.5_largecoll.root'
+haddFilename['runs_C12_Q2_14_large_collimator'] = 'coin_replay_production_C12_thick_14.3_largecoll.root'
+haddFilename['runs_C12_Q2_14_pion_collimator']  = 'coin_replay_production_C12_thick_14.3_smallcoll.root'
+
+haddFilename['runs_LH2_Q2_8']                   = 'coin_replay_production_LH2_8_smallcoll.root'
+haddFilename['runs_LH2_Q2_10_large_collimator'] = 'coin_replay_production_LH2_9.5_largecoll.root'
+haddFilename['runs_LH2_Q2_10_pion_collimator']  = 'coin_replay_production_LH2_9.5_smallcoll.root'
+haddFilename['runs_LH2_Q2_12']                  = 'coin_replay_production_LH2_11.5_largecoll.root'
+haddFilename['runs_LH2_Q2_14_large_collimator'] = 'coin_replay_production_LH2_14.3_largecoll.root'
+haddFilename['runs_LH2_Q2_14_pion_collimator']  = 'coin_replay_production_LH2_14.3_smallcoll.root'
 
 # Where are your hcana-replayed root files?
-rootfileDir = '/home/jmatter/hallc_replay/ROOTfiles'
+rootfileDir = '/volatile/hallc/comm2017/e1206107/ROOTfiles/pass3'
 
 # What's the input root filename template?
 rootfileInputTemplate = 'coin_replay_production_%d_-1.root'
 
-# Output root filename template
-rootfileOutputTemplate = 'CT_coin_%s_%d.root'
+#------------------------------------------------------------------------------- 
+# Loop over kinematics and hadd rootfiles
 
-
-# Loop over combinations of target and Q^2
-kinematics = list(itertools.product(targets, Q2s))
-for k in kinematics:
+# Each key in haddFilename is a runlist.
+for key in haddFilename:
     # Form runlist filename and read it
-    runlist = runlistTemplate % k
-    runlist = os.path.join(runlistDir,runlist)
+    runlist = os.path.join(runlistDir, key)
     f = open(runlist,'r') 
     lines = f.readlines()
 
@@ -46,8 +57,8 @@ for k in kinematics:
         inputRootfiles.append(filename)
 
     # Form output file for hadd
-    outputRootfile = rootfileOutputTemplate % k
-    outputRootfile = os.path.join(rootfileDir, outputRootfile)
+    outputRootfile = os.path.join(rootfileDir, haddFilename[key])
 
     # Run hadd
-    subprocess.call(['hadd'] + [outputRootfile] + inputRootfiles)
+    # subprocess.call(['hadd'] + [outputRootfile] + inputRootfiles)
+    print(['hadd'] + [outputRootfile] + inputRootfiles)
