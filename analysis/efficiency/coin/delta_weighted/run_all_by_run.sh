f8d4f5dbf4b4c0c7ac4286bb3dc8ebca885535f2
John Matter <jcm6fv@virginia.edu>
2020-01-17 17:44:02 -0500

    small change to delta_weighted pid efficiency filenames

---
+++ analysis/efficiency/coin/delta_weighted/run_all_by_run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /site/12gev_phys/softenv.sh 2.3
+cd /home/jmatter/ct_scripts/analysis/efficiency/coin/delta_weighted
+/home/jmatter/ct_scripts/analysis/efficiency/coin/delta_weighted/calculate_all_by_run
