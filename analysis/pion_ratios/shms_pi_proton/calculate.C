34c267ae702f73f8dddf9881dadabde9510d00e9
John Matter <jcm6fv@virginia.edu>
2020-04-21 16:07:05 -0400

    pi/e pi/p ratio work

--- analysis/pion_ratios/shms_pi_proton/calculate.C
+++ analysis/pion_ratios/shms_pi_proton/calculate.C
@@ -9,7 +9,7 @@
 #include <vector>
 #include <map>
 
-// This calculates the number of pi-'s and electrons in the HMS for our data
+// This calculates the number of pi+'s and protons in the HMS for our data
 void calculate() {
     // Load data and cuts
     CTData *data = new CTData("/home/jmatter/ct_scripts/ct_coin_data.json");
@@ -19,20 +19,20 @@ void calculate() {
     std::vector<TString> kinematics = data->GetNames();
 
     // Which cuts?
-    TCut piCut = cuts->Get("hPi");
-    TCut electronCut = cuts->Get("hElec");
+    TCut piCut = cuts->Get("pPi");
+    TCut protonCut = cuts->Get("pProton");
 
-    // Store number of pions and electrons
+    // Store number of pions and protons
     std::map<std::tuple<TString, Int_t>, TEventList*> piLists;
-    std::map<std::tuple<TString, Int_t>, TEventList*> electronLists;
+    std::map<std::tuple<TString, Int_t>, TEventList*> protonLists;
     std::map<std::tuple<TString, Int_t>, Int_t> nPi;
-    std::map<std::tuple<TString, Int_t>, Int_t> nElectron;
+    std::map<std::tuple<TString, Int_t>, Int_t> nProton;
 
     // Where are we saving data?
-    TString csvFilename = "pi_e_count.csv";
+    TString csvFilename = "pi_proton_count.csv";
 
     // ------------------------------------------------------------------------
-    // Loop over kinematics, count pions and electrons
+    // Loop over kinematics, count pions and protons
     TString elistName, elistDraw;
     TString rootFilename;
     TFile *f;
@@ -54,12 +54,12 @@ void calculate() {
             piLists[std::make_tuple(k,run)] = (TEventList*) gDirectory->Get(elistName.Data());
             nPi[std::make_tuple(k,run)] = piLists[std::make_tuple(k,run)]->GetN();
 
-            // Count electrons
-            elistName.Form("%s_run%d_electrons", k.Data(), run);
+            // Count protons
+            elistName.Form("%s_run%d_protons", k.Data(), run);
             elistDraw.Form(">>%s", elistName.Data());
-            T->Draw(elistDraw, electronCut);
-            electronLists[std::make_tuple(k,run)] = (TEventList*) gDirectory->Get(elistName.Data());
-            nElectron[std::make_tuple(k,run)] = electronLists[std::make_tuple(k,run)]->GetN();
+            T->Draw(elistDraw, protonCut);
+            protonLists[std::make_tuple(k,run)] = (TEventList*) gDirectory->Get(elistName.Data());
+            nProton[std::make_tuple(k,run)] = protonLists[std::make_tuple(k,run)]->GetN();
         }
     }
 
@@ -71,7 +71,7 @@ void calculate() {
     ofs.open(csvFilename.Data());
 
     // print header
-    ofs << "kinematics,Q2,target,collimator,run,pions,electrons" << std::endl;
+    ofs << "kinematics,Q2,target,collimator,run,pions,protons" << std::endl;
     for (auto const &k : kinematics) {
         std::cout << "Printing " << k << std::endl;
         for (auto const &run : data->GetRuns(k)) {
@@ -81,7 +81,7 @@ void calculate() {
                 << "," << data->GetCollimator(k) 
                 << "," << run
                 << "," << nPi[std::make_tuple(k,run)]
-                << "," << nElectron[std::make_tuple(k,run)]
+                << "," << nProton[std::make_tuple(k,run)]
                 << std::endl;
         }
     }
