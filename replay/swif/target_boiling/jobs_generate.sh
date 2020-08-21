8a09c65574fd410b07c50751095e002b55c92176
John Matter <jcm6fv@virginia.edu>
2020-08-21 16:49:06 -0400

    add LH2&LD2 boiling replay and move C12 boiling to common replay directory

---
+++ replay/swif/target_boiling/jobs_generate.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Each of these runs files contains the relevant run numbers.
+# Workflow name will be the name of the run files, for consistency.
+# Hopefully this will make re-replaying easy when changes are made
+# to hcana or hallc_replay.
+
+hcswif_dir=/home/jmatter/swif/ctswif/
+runlist_dir=/home/jmatter/ct_scripts/runlists/target_boiling
+
+f=$runlist_dir/shms_lh2_boiling
+f_base=`basename $f`
+$hcswif_dir/hcswif.py --mode replay --spectrometer SHMS_PROD --replay SCRIPTS/SHMS/PRODUCTION/replay_production_all_shms.C --events 1000000 --project c-comm2017 --run file $f --name ${f_base}
+
+f=$runlist_dir/hms_lh2_boiling
+f_base=`basename $f`
+$hcswif_dir/hcswif.py --mode replay --spectrometer HMS_PROD --replay SCRIPTS/HMS/PRODUCTION/replay_production_all_hms.C --events 1000000 --project c-comm2017 --run file $f --name ${f_base}
+
+f=$runlist_dir/shms_ld2_boiling
+f_base=`basename $f`
+$hcswif_dir/hcswif.py --mode replay --spectrometer SHMS_PROD --replay SCRIPTS/SHMS/PRODUCTION/replay_production_all_shms.C --events 1000000 --project c-comm2017 --run file $f --name ${f_base}
+
+f=$runlist_dir/hms_ld2_boiling
+f_base=`basename $f`
+$hcswif_dir/hcswif.py --mode replay --spectrometer HMS_PROD --replay SCRIPTS/HMS/PRODUCTION/replay_production_all_hms.C --events 1000000 --project c-comm2017 --run file $f --name ${f_base}
+
+f=$runlist_dir/shms_carbon_boiling
+f_base=`basename $f`
+$hcswif_dir/hcswif.py --mode replay --spectrometer SHMS_PROD --replay SCRIPTS/SHMS/PRODUCTION/replay_production_all_shms.C --events 1000000 --project c-comm2017 --run file $f --name ${f_base}
+
+f=$runlist_dir/hms_carbon_boiling
+f_base=`basename $f`
+$hcswif_dir/hcswif.py --mode replay --spectrometer HMS_PROD --replay SCRIPTS/HMS/PRODUCTION/replay_production_all_hms.C --events 1000000 --project c-comm2017 --run file $f --name ${f_base}
+
+f=$runlist_dir/coin_carbon_boiling
+f_base=`basename $f`
+$hcswif_dir/hcswif.py --mode replay --spectrometer COIN --replay SCRIPTS/COIN/PRODUCTION/replay_production_ct.C --events -1 --project c-comm2017 --run file $f --name ${f_base}
+
+mv $hcswif_dir/output/*.json json
---
+++ replay/swif/target_boiling/jobs_retry.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+json_dir=/home/jmatter/ct_scripts/replay/swif/target_boiling/json
+for f in `ls $json_dir/*.json`; do
+    workflow_name=`cat $f | python -c 'import sys, json; print json.load(sys.stdin)["name"]'`
+    swif retry-jobs -workflow $workflow_name -regexp ".*"
+    echo
+done
